974187fdf36abaf772c6c1c3fc777a50176f40c8
ElianMartinez <elianmartinez157@gmail.com>
2021-08-12 16:19:25 -0400

    Cambios del maestro de usuarios

--- SistemaTaller/Models/Usuario.cs
+++ SistemaTaller/Models/Usuario.cs
@@ -13,10 +13,17 @@ namespace SistemaTaller.Models
         public Usuario() => Trabajos = new HashSet<Trabajo>();
         [Key]
         public int IdUsuarios { get; set; }
+        [Required(ErrorMessage ="Ingrese el Nombre")]
         public string Nombre { get; set; }
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Ingrese la clave")]
+
         public string Clave { get; set; }
+        [Required(ErrorMessage = "Ingrese cargo")]
+
         public string Cargo { get; set; }
+        [Required(ErrorMessage = "Ingrese el Status")]
+
         public byte? Status { get; set; }
 
         public virtual ICollection<Trabajo> Trabajos { get; set; }
